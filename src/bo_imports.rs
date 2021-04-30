b97d4cd99550af085494e8769feb3c2ac2949d1d
storyfeet <thebeaky@psmail.net>
2021-04-30 17:26:22 +0100

    working on immplementing SSParser for lots bogobble parsers

--- src/bo_imports.rs
+++ src/bo_imports.rs
@@ -10,3 +10,20 @@ impl<CF> SSParser<CF> for EOI {
         it.err_r(Expected::EOI)
     }
 }
+
+impl<CF> SSParser<CF> for &'static str {
+    fn ss_parse<'a>(&self, it: &PIter<'a>, res: &mut String, _: &CF) -> ParseRes<'a, ()> {
+        let mut i2 = it.clone();
+        for x in self.chars() {
+            if i2.next() == Some(x) {
+                return i2.err_rs(self);
+            }
+        }
+        res.push_str(self);
+        Ok((i2, (), None))
+    }
+}
+
+impl<CF, P: SSParser<CF>> SSParser<CF> for KeyWord<P> {
+    fn ss_parse<'a>(&self, it: &PIter<'a>, res: &mut String, _: &CF) -> ParseRes<'a, ()> {}
+}
